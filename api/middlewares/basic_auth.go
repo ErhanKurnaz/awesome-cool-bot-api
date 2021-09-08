9299b8252f064e7c01db94fdf0810e9fbd678b6d
Erhan Kurnaz <36440888+ErhanKurnaz@users.noreply.github.com>
2021-09-08 21:38:18 +0200

    Frontend (#1)
    * added comment
    
    * initial frontend commit
    
    * added a login button
    
    Co-authored-by: Erhan Kurnaz <erhan@globalscreen.nl>

--- api/middlewares/basic_auth.go
+++ api/middlewares/basic_auth.go
@@ -3,6 +3,7 @@ package middlewares
 import "github.com/gin-gonic/gin"
 
 func BasicAuth() gin.HandlerFunc {
+    // TODO: change this with actual auth
 	return gin.BasicAuth(gin.Accounts{
 		"erhan": "kurnaz",
 	})
