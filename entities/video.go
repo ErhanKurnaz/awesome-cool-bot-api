a502285466aefc78f23c91718a4314562342eb4e
Erhan Kurnaz <erhan@globalscreen.nl>
2021-09-08 22:29:29 +0200

    removed files that shouldnt exist

--- entities/video.go
+++
@@ -1,8 +0,0 @@
-package entities
-
-type Video struct {
-	Title       string `json:"title" binding:"required,min=2,max=10" validate:"is-cool"`
-	Description string `json:"description" binding:"required,max=20"`
-	URL         string `json:"url" binding:"required,url"`
-	Author      Person `json:"author" binding:"required"`
-}
